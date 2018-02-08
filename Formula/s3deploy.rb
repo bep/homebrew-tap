66ad596648a8adc3bf4b3485c5c2a52d52bcf3ff
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-08 17:25:44 +0100

    s3deploy version v1.3.1

--- Formula/s3deploy.rb
+++ Formula/s3deploy.rb
@@ -1,9 +1,9 @@
 class S3deploy < Formula
   desc "Deploy static websites to Amazon S3 with Gzip and headers support."
   homepage "https://github.com/bep/s3deploy"
-  url "https://github.com/bep/s3deploy/releases/download/v1.3/s3deploy_1.3_macOS-64bit.tar.gz"
-  version "1.3"
-  sha256 "5883a083f29f4fe0571cfbe79bb6a4dfe965c920c34cbbe1af959532f54451e5"
+  url "https://github.com/bep/s3deploy/releases/download/v1.3.1/s3deploy_1.3.1_macOS-64bit.tar.gz"
+  version "1.3.1"
+  sha256 "252e5c8a70d6ed5cc4f3b27bee3544054fd6c4952a3bd8c7d1eaca6fea605087"
 
   def install
     bin.install "s3deploy"
