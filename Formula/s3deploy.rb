80814d4cc163f7e939e3b327852e0f9399f5e16f
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-07 10:58:58 +0200

    Brew formula update for s3deploy version v2.3.2

--- Formula/s3deploy.rb
+++ Formula/s3deploy.rb
@@ -1,9 +1,9 @@
 class S3deploy < Formula
   desc "Deploy static websites to Amazon S3 with Gzip and headers support."
   homepage "https://github.com/bep/s3deploy"
-  url "https://github.com/bep/s3deploy/releases/download/v2.3.1/s3deploy_2.3.1_macOS-64bit.tar.gz"
-  version "2.3.1"
-  sha256 "a1b1b73cfeeebefd39119be71ce47879bf90073893797807f89deb4aa7823ada"
+  url "https://github.com/bep/s3deploy/releases/download/v2.3.2/s3deploy_2.3.2_macOS-64bit.tar.gz"
+  version "2.3.2"
+  sha256 "480d60eafdce7d796a422a5cbef37326a1a8ebba828ea4c26ff928b9ad887e90"
 
   def install
     bin.install "s3deploy"
