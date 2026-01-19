9146f3f65fff6a7b04eb3f5abb4e015d8e0b3a61
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2026-01-19 11:13:31 +0100

    Update s3deploy to v2.14.0

--- Casks/s3deploy.rb
+++ Casks/s3deploy.rb
@@ -1,15 +1,13 @@
 cask "s3deploy" do
-  version "2.13.0"
-  sha256 "db5e2df739dd286db5ce5009252a860b17daef6601621cf851a811121ce9a42d"
+  version "2.14.0"
+  sha256 "20bfa8dc286580a8f6ca50ef6fce6b5ba65426ca3d14b569ed7892466c4c1ba1"
 
-  url "https://github.com/bep/s3deploy/releases/download/v2.13.0/s3deploy_2.13.0_darwin-universal.pkg"
+  url "https://github.com/bep/s3deploy/releases/download/v2.14.0/s3deploy_2.14.0_darwin-universal.pkg"
   name "s3deploy"
-  desc "A simple tool to deploy static websites to Amazon S3 and CloudFront with Gzip and custom headers support."
+  desc ""
   homepage "https://github.com/bep/s3deploy"
 
-  pkg "s3deploy_2.13.0_darwin-universal.pkg"
-
-  binary "/usr/local/bin/s3deploy"
+  pkg "s3deploy_2.14.0_darwin-universal.pkg"
 
   uninstall pkgutil: "com.bepsays.s3deploy"
 end
