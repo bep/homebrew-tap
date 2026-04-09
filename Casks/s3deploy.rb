c01584f8d137a0f1dcbdfa1bd97f260eea3664f1
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2026-04-09 12:24:55 +0200

    Update s3deploy to v2.16.0

--- Casks/s3deploy.rb
+++ Casks/s3deploy.rb
@@ -1,13 +1,13 @@
 cask "s3deploy" do
-  version "2.15.0"
-  sha256 "09d42e5d2854b9720772355b7317a765c585bfa82231d012e5dff2288802a8aa"
+  version "2.16.0"
+  sha256 "eb666bd7cae4fa8c265fcbfee7357cc32c5262c755f0a28f7cbfcdf03331884d"
 
-  url "https://github.com/bep/s3deploy/releases/download/v2.15.0/s3deploy_2.15.0_darwin-universal.pkg"
+  url "https://github.com/bep/s3deploy/releases/download/v2.16.0/s3deploy_2.16.0_darwin-universal.pkg"
   name "s3deploy"
   desc ""
   homepage "https://github.com/bep/s3deploy"
 
-  pkg "s3deploy_2.15.0_darwin-universal.pkg"
+  pkg "s3deploy_2.16.0_darwin-universal.pkg"
 
   uninstall pkgutil: "com.bepsays.s3deploy"
 end
